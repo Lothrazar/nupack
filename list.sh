ce10531b17cb09aaf3cfd2cc7578376fde3577b6
lothrazar <samson.bassett@gmail.com>
2021-02-01 09:24:44 -0800

    newpack initial with listing script

---
+++ list.sh
@@ -0,0 +1,5 @@
+ls ./mods > mod_list.txt
+ls ./resourcepacks resourcepacks_list.txt
+ls ./shaderpacks shaderpacks_list.txt
+
+
---
+++ scripts/list.sh
@@ -0,0 +1,9 @@
+rm -f mods_list.txt
+rm -f resourcepacks_list.txt
+rm -f shaderpacks_list.txt
+
+ls ./mods > mods_list.txt
+ls ./resourcepacks > resourcepacks_list.txt
+ls ./shaderpacks > shaderpacks_list.txt
+
+echo "listing complete"
