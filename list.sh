29085b3cbbd9acffb0681b0618cc2945c01db600
lothrazar <samson.bassett@gmail.com>
2021-02-06 12:21:48 -0800

    list script fix and rerun

--- list.sh
+++ list.sh
@@ -1,5 +1,11 @@
-ls ./mods > mod_list.txt
+rm -f mods_list.txt
+rm -f resourcepacks_list.txt
+rm -f shaderpacks_list.txt
+
+touch mods_list.txt
+touch resourcepacks_list.txt
+touch shaderpacks_list.txt
+
+ls ./mods > mods_list.txt
 ls ./resourcepacks resourcepacks_list.txt
 ls ./shaderpacks shaderpacks_list.txt
-
-
